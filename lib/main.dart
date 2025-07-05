44882fdaa6139137f8d16538ef5c2ddb061477e3
Mahmoud Awad <awad200770@outlook.com>
2025-07-05 18:16:43 +0300

    testing custome widgets

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:bingo/Alshimaa.dart';
+import 'package:bingo/config/theme_app.dart';
 import 'package:bingo/mahmoud.dart';
 import 'package:flutter/material.dart';
 
@@ -13,6 +14,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
+      theme: appThemne,
       home: Scaffold(
         body: Center(
           child: Builder(
