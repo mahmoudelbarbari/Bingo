37078e2d8a1b79e6b74a1643cbc43d1b7ef010b2
Mahmoud Awad <awad200770@outlook.com>
2025-07-06 19:24:03 +0300

    creating app routes in seperate file

---
+++ lib/app/routes/app_routes.dart
@@ -0,0 +1,12 @@
+import 'package:bingo/features/onboarding/presentation/pages/splash_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:bingo/features/onboarding/presentation/pages/onboarding_screen1.dart';
+import 'package:bingo/features/onboarding/presentation/pages/onboarding_screen2.dart';
+import 'package:bingo/features/onboarding/presentation/pages/onboarding_screen3.dart';
+
+final Map<String, WidgetBuilder> appRoutes = {
+  '/splashScreen': (context) => const WelcomeSplashScreen(),
+  '/onboarding1': (context) => const OnboardingScreen1(step: 0),
+  '/onboarding2': (context) => const OnboardingScreen2(step: 1),
+  '/onboarding3': (context) => const OnboardingScreen3(step: 2),
+};
